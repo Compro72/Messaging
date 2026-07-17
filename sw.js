3c42844a92f84225db29f333e1cb5c38af82710e
Compro72 <153555237+Compro72@users.noreply.github.com>
2026-07-16 22:58:02 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'p2p-lobby-v1';
+// Using relative paths so GitHub Pages subfolders don't break the cache
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
